51c28fd719e316400021c38473ebe4e5f144dcef
V-Senex <junior.07.inator@gmail.com>
2021-03-29 16:20:35 +0300

    TextGenerator minor changes

--- src/TextGenerator/TextGenerator.java
+++ src/TextGenerator/TextGenerator.java
@@ -1,4 +1,17 @@
 package TextGenerator;
 
+import java.io.File;
+
 public class TextGenerator {
+    public TextGenerator(File txt) {
+
+    }
+    public TextGenerator(String pathToTxt) {
+
+    }
+
+    public String getText(int length) {
+        return null;
+    }
+
 }
--- src/TextGenerator/parsers/TxtParser.java
+++ src/TextGenerator/parsers/TxtParser.java
@@ -5,16 +5,5 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class TxtParser {
-    public static String getText(String pathToTxt) {
-        StringBuilder text = new StringBuilder();
-        try (BufferedReader buf = new BufferedReader(new FileReader(pathToTxt))) {
-            String line;
-            while ((line = buf.readLine()) != null) {
-               text.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return text.toString();
-    }
+
 }
