edad5554b4bab57f3a269d022b5b37e64701767a
V-Senex <junior.07.inator@gmail.com>
2021-03-29 18:17:36 +0300

    TextGenerator minimal sufficient basics done

--- src/TextGenerator/Tester.java
+++ src/TextGenerator/Tester.java
@@ -5,32 +5,10 @@ import TextGenerator.handlers.FilesHandler;
 import java.io.File;
 
 public class Tester {
-    File jumoreski = FilesHandler.getFile("jumoreski.txt");
+    private static final File JUMORESKI = FilesHandler.getFile("jumoreski.txt");
 
     public static void main(String[] args) {
-
-        TextGenerator.Word w = new TextGenerator.Word("a");
-        w.addNextWord("b");
-        w.addNextWord("b");
-        w.addNextWord("b");
-        w.addNextWord("c");
-        w.addNextWord("c");
-        w.addNextWord("e");
-        w.addNextWord("e");
-        w.addNextWord("f");
-
-
-
-        /*
-        XslxParser.parseToTxt(
-                "src/TextGenerator/data/sources/jumoreski.xlsx",
-                "src/TextGenerator/data/lists/jumoreski.txt");
-
-        TxtCleaner.deleteIfContains(
-                TxtCleaner.defaultList,
-                "src/TextGenerator/data/lists/jumoreski.txt");
-
-        System.out.println(Reader.readTxt(PATH_JUMORESKI_TXT));
-         */
+        TextGenerator textGenerator = new TextGenerator(JUMORESKI);
+        System.out.println(textGenerator.getText(100));
     }
 }
--- src/TextGenerator/TextGenerator.java
+++ src/TextGenerator/TextGenerator.java
@@ -12,59 +12,117 @@ public class TextGenerator {
 
     public TextGenerator(String sourceTxtPath) {
         sourceText = Reader.readTxt(sourceTxtPath);
-        words = parseWordsFromTxt();
+        parseWordsFromTxt();
     }
 
     public TextGenerator(File sourceTxtFile) {
         this(sourceTxtFile.getPath());
     }
 
-    // TODO
     public String getText(int length) {
-        return null;
+        StringBuilder text = new StringBuilder();
+        Word current = getFirstWord();
+        for (int i = 0; i < length; i++) {
+            text.append(current.getWord() + " ");
+            current = findWord(current.getNextWord());
+        }
+        return text.toString();
     }
 
-    // TODO
-    private ArrayList<Word> parseWordsFromTxt() {
-        ArrayList<Word> words = new ArrayList<>();
-        return words;
+    private Word getFirstWord() {
+        ArrayList<Word> capital = new ArrayList<>();
+        for (Word word : words) {
+            if (word.getWord().length() > 0 && word.getWord().charAt(0) > 'A' && word.getWord().charAt(0) < 'Я') {
+                capital.add(word);
+            }
+        }
+        return capital.get(new Random().nextInt(capital.size()));
     }
 
-    static class Word {
-        private String word;
+    private void parseWordsFromTxt() {
+        String[] textWords = sourceText.split(" ");
+
+        String previousWord = textWords[0];
+        addWord(previousWord);
+        for (int i = 1; i < textWords.length; i++) {
+            String currentWord = textWords[i];
+            updateWords(previousWord, currentWord);
+            previousWord = currentWord;
+        }
+    }
+
+    private void updateWords(String previous, String current) {
+        findWord(previous).addNextWord(current);
+        addWord(current);
+    }
+
+    // returns link to word in the list
+    private Word findWord(String word) {
+        for (Word w : words) {
+            // word already exists
+            if (w.getWord().equals(word)) {
+                return w;
+            }
+        }
+        // word not found
+        Word newWord = new Word(word);
+        words.add(newWord);
+        return newWord;
+    }
+
+    private void addWord(String word) {
+        for (Word w : words) {
+            // word already exists
+            if (w.getWord().equals(word)) {
+                return;
+            }
+        }
+        // word not found
+        Word newWord = new Word(word);
+        words.add(newWord);
+    }
+
+    @Override
+    public String toString() {
+        return "TextGenerator{" +
+                "words=" + words +
+                '}';
+    }
+
+    private static class Word {
+        private final String word;
         private final ArrayList<NextWord> nextWords = new ArrayList<>();
 
-        public Word(String word) {
+        private Word(String word) {
             this.word = word;
         }
 
-        void addNextWord(String word) {
-            for(NextWord nextWord : nextWords) {
+        private void addNextWord(String word) {
+            for (NextWord nextWord : nextWords) {
                 // word already exists
-                if(nextWord.getNextWord().equals(word)) {
+                if (nextWord.getNextWord().equals(word)) {
                     nextWord.incrementCounter();
                     return;
                 }
             }
-
             // word not found
             NextWord newNextWord = new NextWord(word);
             newNextWord.incrementCounter();
             nextWords.add(newNextWord);
         }
 
-        String getNextWord() {
+        private String getNextWord() {
             int total = 0;
             // counting total weight
-            for(NextWord nextWord : nextWords) {
+            for (NextWord nextWord : nextWords) {
                 total += nextWord.getCounter();
             }
             // probability distribution depends on frequency of word occurrence
             int result = new Random().nextInt(total) + 1;
             // getting randomly chosen word
-            for(NextWord nextWord : nextWords) {
+            for (NextWord nextWord : nextWords) {
                 result -= nextWord.getCounter();
-                if(result <= 0) {
+                if (result <= 0) {
                     return nextWord.getNextWord();
                 }
             }
@@ -72,31 +130,35 @@ public class TextGenerator {
             return null;
         }
 
+        private String getWord() {
+            return word;
+        }
+
         @Override
         public String toString() {
-            return "Word{" +
+            return "\nWord{" +
                     "word='" + word + '\'' +
                     ", nextWords=" + nextWords +
                     '}';
         }
 
-        private class NextWord {
+        private static class NextWord {
             private final String nextWord;
             private int counter;
 
-            public NextWord(String nextWord) {
+            private NextWord(String nextWord) {
                 this.nextWord = nextWord;
             }
 
-            void incrementCounter() {
+            private void incrementCounter() {
                 counter++;
             }
 
-            public String getNextWord() {
+            private String getNextWord() {
                 return nextWord;
             }
 
-            public int getCounter() {
+            private int getCounter() {
                 return counter;
             }
 
@@ -109,5 +171,4 @@ public class TextGenerator {
             }
         }
     }
-
 }
