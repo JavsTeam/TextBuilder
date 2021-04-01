682aebe48ea9ccd73a9f9badb7dedb3b717697ee
V-Senex <vdm.snx@gmail.com>
2021-04-01 10:47:49 +0300

    TextGenerator directory support add

--- src/TextGenerator/handlers/FilesHandler.java
+++ src/TextGenerator/handlers/FilesHandler.java
@@ -5,54 +5,70 @@ import java.io.IOException;
 import java.nio.file.FileSystemException;
 
 public class FilesHandler {
-    private static final File SRC_DIR = new File("src");
-    private static final File FILES_DIR = new File("src/TextGenerator/data/files");
-
     public static File getFile(String fileName) {
         File file;
-        if ((file = recursiveSearch(SRC_DIR, fileName)) != null) {
-            return file;
-        } else {
-            return create(fileName);
-        }
+        return (file = recursiveFileSearch(Dir.SRC.get(), fileName)) != null ?
+                file : createFile(fileName);
     }
 
     public static String getPath(String fileName) {
-        File file;
-        if ((file = recursiveSearch(SRC_DIR, fileName)) != null) {
-            return file.getPath();
-        } else {
-            return create(fileName).getPath();
-        }
+        return getFile(fileName).getPath();
     }
 
-    public static File create(String fileName, String pathToDirectory) {
+    public static File createFile(String fileName, String pathToDirectory) {
         File newFile = new File(pathToDirectory + "/" + fileName);
         try {
             if (newFile.createNewFile()) return newFile;
             throw new FileSystemException("Unacceptable file name: " + fileName);
-        } catch (IOException e ) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static File create(String fileName, File directory) {
-        return create(fileName, directory.getPath());
+    public static File createFile(String fileName, File directory) {
+        return createFile(fileName, directory.getPath());
     }
 
-    public static File create(String fileName) {
-        return create(fileName, FILES_DIR.getPath());
+    public static File createFile(String fileName) {
+        return createFile(fileName, Dir.FILES.get().getPath());
     }
 
-    public static boolean isExist(String fileName) {
-        return recursiveSearch(SRC_DIR, fileName) != null;
+    public static boolean isFileExist(String fileName) {
+        return recursiveFileSearch(Dir.SRC.get(), fileName) != null;
     }
 
-    private static File recursiveSearch(File current, String fileName) {
+    public static boolean isDirExist(String dirName) {
+        return recursiveDirSearch(Dir.SRC.get(), dirName) != null;
+    }
+
+    public static File getDirectory(String dirName) {
+        File file;
+        return (file = recursiveDirSearch(Dir.SRC.get(), dirName)) != null ?
+                file : createDirectory(dirName, Dir.DATA.get().getPath());
+    }
+
+    public static File createDirectory(String dirName, String pathToSource) {
+        File newFile = new File(pathToSource + "/" + dirName);
+        File dir;
+        if (isDirExist(dirName) && (dir = getDirectory(dirName)).getPath().equals(newFile.getPath())) {
+            return dir;
+        }
+        if (newFile.mkdir()) {
+            return newFile;
+        }
+        try {
+            throw new FileSystemException("Unacceptable directory name or invalid path");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static File recursiveFileSearch(File current, String fileName) {
         if (current.isDirectory()) {
             for (File content : current.listFiles()) {
-                File result = recursiveSearch(content, fileName);
+                File result = recursiveFileSearch(content, fileName);
                 if (result != null) {
                     return result;
                 }
@@ -60,6 +76,39 @@ public class FilesHandler {
             return null;
         } else {
             return current.getName().equals(fileName) ? current : null;
+        }
+    }
+
+    private static File recursiveDirSearch(File current, String dirName) {
+        if (current.isDirectory()) {
+            if (current.getName().equals(dirName)) {
+                return current;
+            }
+            for (File content : current.listFiles()) {
+                File result = recursiveDirSearch(content, dirName);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public enum Dir {
+        SRC(new File("src")),
+        PROJECT(getDirectory("TextGenerator")),
+        DATA(getDirectory("data")),
+        PROCESSED(getDirectory("processed")),
+        FILES(getDirectory("files"));
+
+        File dir;
+
+        Dir(File dir) {
+            this.dir = dir;
+        }
+
+        public File get() {
+            return dir;
         }
     }
 }
