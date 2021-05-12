06c57f09dc6d02706679fd1aeb87d4f4b7cf3155
binocla <tdoktora@inbox.ru>
2021-05-12 20:52:22 +0300

    Done REST length & depth. Check README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 To compile project
 -----------------
 ### Just run Dockerfile :P
-#### Classic IP: 0.0.0.0:32769->8080/tcp
+Classic IP: 0.0.0.0:32769->8080/tcp
+------------
+### Local run:
+#### Run Application & localhost:8080/jumoreski (or bugurts) ?length=*custom*&depth=*custom*
+#### Default values ar 1000 & 1
 
--- src/main/java/TextGenerator/Tester.java
+++ src/main/java/TextGenerator/Tester.java
@@ -1,27 +1,40 @@
 package TextGenerator;
 
 import TextGenerator.handlers.Files;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 
 
 @RestController
+@Log
 public class Tester {
     private static final File JUMORESKI = Files.getFile("jumoreski.txt");
     private static final File BUGURTS = Files.getFile("bugurts.txt");
 
     @GetMapping("/jumoreski")
-    public static String jumoreski() {
-       TextGenerator textGenerator = new TextGenerator(1, JUMORESKI);
-       return textGenerator.getText(1000);
+    public static String jumoreski(@RequestParam(value = "length", defaultValue = "1000") String length,
+                                   @RequestParam(value = "depth", defaultValue = "1") String depth) {
+        if (Integer.parseInt(depth) > 2) {
+            depth = "2";
+        }
+        TextGenerator textGenerator = new TextGenerator(Integer.parseInt(depth), JUMORESKI);
+        log.info("Current depth of jumoreski is " + depth);
+        log.info("Current length of jumoreski is " + length);
+        return textGenerator.getText(Integer.parseInt(length));
     }
     @GetMapping("/bugurts")
-    public static String bugurts() {
-        TextGenerator textGenerator = new TextGenerator(1, BUGURTS);
-        return textGenerator.getText(1000);
+    public static String bugurts(@RequestParam(value = "length", defaultValue = "1000") String length,
+                                 @RequestParam(value = "depth", defaultValue = "1") String depth) {
+        if (Integer.parseInt(depth) > 2) {
+            depth = "2";
+        }
+        TextGenerator textGenerator = new TextGenerator(Integer.parseInt(depth), BUGURTS);
+        log.info("Current depth of bugurts is " + depth);
+        log.info("Current length of bugurts is " + length);
+        return textGenerator.getText(Integer.parseInt(length));
     }
 }
